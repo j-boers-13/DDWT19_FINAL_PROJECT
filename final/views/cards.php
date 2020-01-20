c80cda117babc5f898edcd452d91c4fb4e05b9b5
Quin <q.n.kroon@student.rug.nl>
2020-01-20 21:09:56 +0100

    changed formatting of buttons

--- final/views/cards.php
+++ final/views/cards.php
@@ -1,25 +1,24 @@
 <!-- Room count -->
-<!-- Room count -->
 <div class="card">
     <div class="card-body">
-        <p class="count">We currently have</p>
-        <h2><?= $nbr_rooms ?></h2>
-        <p>rooms listed.</p>
-        <a href="/DDWT19_FINAL_PROJECT/final/add/" class="btn btn-primary">Add yours!</a>
+        <p class="count">We currently have <span class = "emphasis"> <?= $nbr_rooms ?> </span> rooms listed. </p>
+        <div class = "pd-15">
+        <a href="/DDWT19_FINAL_PROJECT/final/add/" class="btn btn-success">Add yours!</a>
+        </div>
     </div>
 </div>
 <!-- Users count -->
 <div class="card">
     <div class="card-body">
-        <p class="count">We currently have
-        <div class = "emphasis"> <?= $nbr_users ?> </div>
-        registered users, of which
-        <div class = "emphasis"><?=$nbr_tenants?> </div>
-        are tenants, and
-        <div class = "emphasis"><?=$nbr_owners?> </div>
-        are owners. </p>
+        <p class="count">
+            We currently have <span class = "emphasis"> <?= $nbr_users ?> </span> registered users, of which
+            <span class = "emphasis"> <?=$nbr_tenants?> </span> are tenants, and <span class = "emphasis"><?=$nbr_owners?>
+            </span> are owners.
+        </p>
         <?php if(isset($user_id)){ ?>
-        <a href="/DDWT19_FINAL_PROJECT/final/register/" class="btn btn-primary">Join now!</a>
+        <div class = "pd-15">
+        <a href="/DDWT19_FINAL_PROJECT/final/register/" class="btn btn-success">Join now!</a>
+        </div>
         <?php } ?>
     </div>
 </div>
